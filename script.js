6c17e13623dd7c9c2018248d74774dc1e1e5f44a
Youry Gladkov <dyger3579@gmail.com>
2025-07-21 13:54:00 +0300

    add bureger menu

--- script.js
+++ script.js
@@ -32,3 +32,13 @@ breakpoints: {
   }
  
 });
+
+  document.addEventListener('DOMContentLoaded', function () {
+    const burger = document.querySelector('.burger');
+    const menu = document.querySelector('.menu');
+
+    burger.addEventListener('click', function () {
+      burger.classList.toggle('active');
+      menu.classList.toggle('open');
+    });
+  })
